cb54c98be2b4332a83d350090eaec811ee4b3b3c
DigitalDominance <digitaldominanceseo@gmail.com>
2025-02-04 20:35:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WalletConnectModal from './components/WalletConnectModal';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import CreateRaffle from './pages/CreateRaffle';
 import RaffleDetail from './pages/RaffleDetail';
 import Profile from './pages/Profile';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import './styles.css';
 
 function App() {
   const [wallet, setWallet] = useState(null);
@@ -22,14 +24,17 @@ function App() {
     <Router>
       {!wallet && <WalletConnectModal onConnect={handleWalletConnect} />}
       {wallet && (
-        <div>
+        <div className="app-container">
           <Navbar wallet={wallet} onDisconnect={handleWalletDisconnect} />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/create" element={<CreateRaffle wallet={wallet} />} />
-            <Route path="/raffle/:raffleId" element={<RaffleDetail wallet={wallet} />} />
-            <Route path="/profile" element={<Profile wallet={wallet} />} />
-          </Routes>
+          <div className="content-container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/create" element={<CreateRaffle wallet={wallet} />} />
+              <Route path="/raffle/:raffleId" element={<RaffleDetail wallet={wallet} />} />
+              <Route path="/profile" element={<Profile wallet={wallet} />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
       )}
     </Router>
